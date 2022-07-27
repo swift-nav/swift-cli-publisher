e302233ad1499edb195c4625efdd71c90a38d993
adrian-kong <adrian.k1234567890@gmail.com>
2022-07-27 10:54:21 +1000

    modified template, added web meta

--- link_release.sh
+++ link_release.sh
@@ -3,28 +3,47 @@ TEMPLATE_FILE="template.json"
 
 echo "Generating new $VERSION release for $NAME"
 
+# Should probably use serialization from Package struct
 NEW_RELEASE=$(jq \
   --arg name "$NAME" \
   --arg version "$VERSION" \
+  \
   --arg base_dir_linux "${BASE_DIR[0]}" \
   --arg base_dir_macos "${BASE_DIR[0]}" \
   --arg base_dir_windows "${BASE_DIR[0]}" \
+  \
+  --arg base_url "${BASE_URL}" \
   --arg project_slug "${PROJECT_SLUG:="swift-nav/$NAME"}" \
   --arg download_linux "${DOWNLOAD_FILES[0]}" \
   --arg download_macos "${DOWNLOAD_FILES[1]}" \
   --arg download_windows "${DOWNLOAD_FILES[2]}" \
+  \
   --arg tools "${TOOLS}" \
   --arg linked "${LINKED:=false}" \
   --compact-output \
   '.name=$name
   | .version=$version
+
   | .base_dir.linux=$base_dir_linux
   | .base_dir.macos=$base_dir_macos
   | .base_dir.windows=$base_dir_windows
-  | .download.GitHub.project_slug=$project_slug
-  | .download.GitHub.linux=$download_linux
-  | .download.GitHub.macos=$download_macos
-  | .download.GitHub.windows=$download_windows
+
+  | if .download.Web.base_url=="" then
+  (
+    .download.GitHub.project_slug=$project_slug
+    | .download.GitHub.linux=$download_linux
+    | .download.GitHub.macos=$download_macos
+    | .download.GitHub.windows=$download_windows
+  )
+  else
+  (
+    .download.Web.base_url=$base_url
+    | .download.Web.linux=$download_linux
+    | .download.Web.macos=$download_macos
+    | .download.Web.windows=$download_windows
+  )
+  end
+
   | .tools=[$tools]
   | .linked=$linked' \
   $TEMPLATE_FILE)
